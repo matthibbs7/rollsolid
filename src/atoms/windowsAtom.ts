4b2137a01546b22d8ba3a72673d6a23efa95bdcf
Matthew Hibbs <89611778+matthibbs7@users.noreply.github.com>
2023-06-02 01:36:34 -0400

    Widget dropdown dev

--- src/atoms/windowsAtom.ts
+++ src/atoms/windowsAtom.ts
@@ -21,7 +21,7 @@ const defaultWindowsState: WindowsState = {
         isMinimizied: false,
         settingsOpen: false,
         handleColor: '#121212', //TODO # default #121212 -> overridden
-        textContent: '<h1>Welcome to Rollsolid</h1><p>This is a <b>notes</b> widget.</p><p></p><p>You can load additional widgets to your <i>dashboard</i>.</p><p></p><code>p.s. you can edit this text!</code><p></p>',
+        textContent: '<h1>Welcome to Rollsolid</h1><p></p><p>This is a <b>notes</b> widget.</p><p>You can load additional widgets to your <i>dashboard</i>.</p><p></p><code>p.s. you can edit this text!</code><p></p>',
     }],
 };
 
--- src/components/Widget/WidgetNavbar/WidgetNavbar.tsx
+++ src/components/Widget/WidgetNavbar/WidgetNavbar.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { Button, Text, Flex, useColorMode, Menu, MenuButton, MenuItem, MenuList, Divider, useToast } from '@chakra-ui/react';
-import { MdOutlineTimer, MdOutlineWidgets } from 'react-icons/md';
-import { FaRegStickyNote } from 'react-icons/fa';
+import { Button, Text, Flex, useColorMode, Menu, MenuButton, MenuItem, MenuList, useToast } from '@chakra-ui/react';
+import { MdAddChart, MdLibraryBooks, MdOutlineWidgets, MdStackedLineChart } from 'react-icons/md';
 import { WindowState } from '@/types/windows';
 import { useRecoilState } from 'recoil';
 import { windowsState } from '@/atoms/windowsAtom';
@@ -90,10 +89,40 @@ const WidgetNavbar:React.FC = () => {
                                 <Text pt={0.5} pl={2.5} color='#A3A3A3' fontSize='12px' fontWeight={400} borderRadius="0px">Widgets</Text>    
                             </Flex>
                         </MenuButton>
-                        <MenuList w="600px" minW="0" h="236px" mt={1.5} p="1px" py={0} bg="#161616" border="1px solid #494D51" borderRadius="0" motionProps={{
+                        <MenuList w="600px" minW="0" h="224px" mt={1.5} p="1px" py={0} bg="#161616" border="1px solid #494D51" borderRadius="6px" motionProps={{
                             transition: { duration: 0 },
                             animate: 'visible'}}>
-                            <Flex direction='row' ml={2} px={2} py={4}>
+                            <Flex direction='column' w='100%' h='100%' ml={2} py={0}>
+                                <MenuItem w='44%' h='48px' mt={2} px={3} color='#A3A3A3' bg='none' borderRadius='6px' _hover={{cursor: 'pointer', bg: '#111111'}} onClick={() => addNotesWidget()}>
+                                    <MdLibraryBooks color='#a3a3a3' fontSize='28px' />
+                                    <Flex direction='column' ml={3}>
+                                        <Text color='#d1d1d1' fontSize='12px'>Notes</Text>
+                                        <Text color='#a3a3a3' fontSize='11px'>Document your opponents behavior</Text>
+                                    </Flex>
+                                </MenuItem>
+                                <Flex align='center' w='44%' h='48px' px={3} color='#A3A3A3' borderRadius='6px' _hover={{cursor: 'pointer', bg: '#111111'}}>
+                                    <MdOutlineWidgets fontSize='28px' color='#A3A3A3' />
+                                    <Flex direction='column' ml={3}>
+                                        <Text color='#d1d1d1' fontSize='12px'>EQ Calculator</Text>
+                                        <Text color='#a3a3a3' fontSize='11px'>Estimate GTO per hand</Text>
+                                    </Flex>
+                                </Flex>
+                                <Flex align='center' w='44%' h='48px' px={3} color='#A3A3A3' borderRadius='6px' _hover={{cursor: 'pointer', bg: '#111111'}}>
+                                    <MdAddChart fontSize='28px' color='#A3A3A3' />
+                                    <Flex direction='column' ml={3}>
+                                        <Text color='#d1d1d1' fontSize='12px'>Ranges</Text>
+                                        <Text color='#a3a3a3' fontSize='11px'>100+ scenario specific range charts</Text>
+                                    </Flex>
+                                </Flex>
+                                <Flex align='center' w='44%' h='48px' px={3} color='#A3A3A3' borderRadius='6px' _hover={{cursor: 'pointer', bg: '#111111'}}>
+                                    <MdStackedLineChart fontSize='28px' color='#A3A3A3' />
+                                    <Flex direction='column' ml={3}>
+                                        <Text color='#d1d1d1' fontSize='12px'>Betting Odds</Text>
+                                        <Text color='#a3a3a3' fontSize='11px'>Calculator for estimating pot odds</Text>
+                                    </Flex>
+                                </Flex>
+                            </Flex>
+                            {/* <Flex direction='row' ml={2} px={2} py={4}>
                                 <Flex direction='column' w='33%'>
                                     <Text mb={4} ml={0} pl={2} py={0.5} color='#C7AE7A' fontFamily='Inter' fontSize='13px' fontWeight={500} fontStyle='italic' border='1px solid grey'>Probability</Text>
                                     <Divider />
@@ -182,8 +211,8 @@ const WidgetNavbar:React.FC = () => {
                                     </Flex> 
                                 </Flex>
                                   
-                            </Flex>
-                            <Text mt={-1} ml={4} color='#7083B3' fontSize='14px' fontWeight={500}>Select a widget above to load it on your dashboard.</Text> 
+                            </Flex> */}
+                            {/* <Text mt={-1} ml={4} color='#7083B3' fontSize='14px' fontWeight={500}>Select a widget above to load it on your dashboard.</Text>  */}
                         </MenuList>
                     </>
                 )}
